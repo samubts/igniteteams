919779cf514466175c55531088693dba10fc4663
Samuel Batista <samuelbts1993@gmail.com>
2024-07-08 15:23:08 -0300

    Estudos 08/07/2024

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
-import { ThemeProvider } from 'styled-components';
-
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 import theme from './src/theme';
-
 import { Groups } from '@screens/Groups';
 
-
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor={'transparent'}
+        translucent
+      />
+      { fontsLoaded ? <Groups /> : < Loading/>}
     </ThemeProvider>
   );
 }
