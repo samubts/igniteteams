547d1b778ff6da6e9067c655aa9823d5ed33239b
Samuel Batista <samuelbts1993@gmail.com>
2024-07-15 17:54:46 -0300

    Estudos 1507/2024.2

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
 import { Loading } from '@components/Loading';
 import theme from './src/theme';
-import { Groups } from '@screens/Groups';
-import { NewGroup } from '@screens/NewGroup';
+import { Players } from '@screens/Players';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor={'transparent'}
         translucent
       />
-      { fontsLoaded ? <Groups /> : < Loading/>}
+      { fontsLoaded ? <Players /> : < Loading/>}
     </ThemeProvider>
   );
 }
---
+++ src/screens/Players/index.tsx
@@ -0,0 +1,27 @@
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { Input } from '@components/input';
+
+import { Container, Form } from './styles';
+
+export function Players() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Highlight 
+        title='Nome da turma'
+        subtitle='Adicione a galera e separe os times'
+      />
+
+      <Form>
+        <Input 
+          placeholder='Nome da pessoa'
+          autoCorrect={false}
+        />
+        <ButtonIcon icon='add'/>
+      </Form>
+    </Container>
+  );
+}
